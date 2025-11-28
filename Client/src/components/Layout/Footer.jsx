868b6852695399ad68ed0905db395c63771d7fbd
Johnjerry <johnjerry8749@gmail.com>
2025-11-28 09:04:21 -0800

    Refactor getExchangeRate function to improve error handling and fallback logic

--- Client/src/components/Layout/Footer.jsx
+++ Client/src/components/Layout/Footer.jsx
@@ -142,7 +142,7 @@ const Footer = () => {
               </li>
               <li className="contact-item">
                 <i className="bi bi-geo-alt-fill text-success me-2"></i>
-                <span className="text-grey">Usa</span>
+                <span className="text-grey">USA</span>
               </li>
             </ul>
 
